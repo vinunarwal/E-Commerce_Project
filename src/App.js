5749339a0d785e9b42e95081068f56e572ba6f7b
SahilSheorn <sahilks0317@gmail.com>
2023-11-23 15:46:18 +0530

    contact

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import Navbar from'./components/Navbar';
-import Wishlist from './components/wishlist';
-// import Contact from './components/contact';
+// import Wishlist from './components/wishlist';
+import Contact from './components/contact';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Navbar/>
-        <Wishlist/>
+        {/* <Wishlist/> */}
+        <Contact/>
 
       </header>
     </div>
