26cd14db82124520392607723e52fd4100e6f91e
gauravsaini98966 <gs9042312@gmail.com>
2023-12-12 16:58:11 +0530

    new

---
+++ src/Layout.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Navbar from './F_Website/Navbar'
+import Fother from './F_Website/Fother'
+import { Outlet } from 'react-router-dom'
+
+function Layout() {
+  return (
+    <div>
+      <Navbar/>
+      <Outlet/>
+      <Fother/>
+
+    </div>
+  )
+}
+
+export default Layout;
--- src/index.js
+++ src/index.js
@@ -2,9 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-// import reportWebVitals from './reportWebVitals';
+import reportWebVitals from './reportWebVitals';
 // import  '../src/Web.css'
 // import '../src/index_vinu.css';
+
+
+
+import { Route,RouterProvider,createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
+import Layout from './Layout';
+import Home from './F_Website/Home'
+// import Home from './Component/Home';
+// import About from '../src/Component/About'
+// import Contact from './Component/Contact';
+// import User  from './Component/User';
+// import Github from '../src/Component/Github'; 
+
 
 
 const router =createBrowserRouter(
@@ -14,9 +26,7 @@ const router =createBrowserRouter(
 
   <Route path='/' element={<Home/>} />
   <Route path='Home' element={<Home/>} /> 
-      {/* <Route path='contact' element={<Contact/>} />  */}
-      {/* <Route path='About' element={<About/>} />  */}
-    
+      
 
     </Route>
   )
@@ -25,12 +35,12 @@ const router =createBrowserRouter(
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  // <React.StrictMode>
-    <App />
-  // </React.StrictMode>
+<React.StrictMode>
+    <RouterProvider router={router}/>
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
+reportWebVitals();
